bb8ade05fd4d0497d728ea900269d2ebeb4f04e1
shina <rasheedshinaopeyemi@gmail.com>
2020-06-27 23:47:19 +0100

    added the navbar components and its children components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `gatsby restaurant`,
+    description: `a restaurant site built with gatsbyjs and contentful.`,
+    author: `shina`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +15,8 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,11 +26,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { createGlobalStyle } from "styled-components"
+// we are able to do this because we made navbar the entry point in that folder
+import Navbar from "../components/Globals/Navbar"
 
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
+      <Navbar />
       <main>{children}</main>
     </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Open sans', sans-serif;
+  color:#262626;
+  background:#fff;
+}
+
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
