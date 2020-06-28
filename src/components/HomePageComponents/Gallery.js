cdec6c9201d1c51b224e0650c8358a2ae7021c7b
shina <rasheedshinaopeyemi@gmail.com>
2020-06-28 23:08:58 +0100

    added the gallery items and used grid css to create the layout

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -60,6 +60,52 @@ const Gallery = () => {
   )
 }
 
-const GalleryWrapper = styled.div``
+const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 1rem;
+  .item {
+    position: relative;
+  }
+
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: ${styles.colors.mainYellow};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+  }
+
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 992px) {
+    .gastby-image-wrapper {
+      height: 100%;
+    }
+    grid-template-areas:
+      "one one two"
+      "one one three";
+
+    .item-1 {
+      grid-area: one;
+    }
+
+    .item-2 {
+      grid-area: two;
+    }
+
+    .item-3 {
+      grid-area: three;
+    }
+  }
+`
 
 export default Gallery
